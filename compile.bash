e31dc0f36975868bc6e9b17c6bdbbd3b2a3f921f
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-06 21:28:26 -0700

    c file

--- compile.bash
+++ compile.bash
@@ -1,1 +1,1 @@
-gcc `pkg-config --cflags gtk+-3.0` -o music_client_gtk music_client_gtk.c `pkg-config --libs gtk+-3.0`
+gcc `pkg-config --cflags gtk+-3.0` -o music_client_gtk Mu2c.c `pkg-config --libs gtk+-3.0`
