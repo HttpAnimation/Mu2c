82908d1306eb141a23def607d82e25805977f5b8
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-08 22:00:57 -0700

    compile data and time

--- compile.bash
+++ compile.bash
@@ -3,5 +3,5 @@
 # Compile the program
 gcc `pkg-config --cflags gtk+-3.0` -o Mu2c Mu2c.c `pkg-config --libs gtk+-3.0`
 
-# Add date and time to Version.ini
-echo "$(date +'%Y-%m-%d %H:%M:%S')" >> Version.ini
+# Remove old contents and add date and time to Version.ini
+echo "$(date +'%Y-%m-%d %H:%M:%S')" > Version.ini
