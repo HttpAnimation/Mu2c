a8f6e8fd049f1ce69246dc9869adc8fbb743f35a
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-08 22:12:08 -0700

    mu2c

--- compile.bash
+++ compile.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile the program
-gcc -o music_player Mu2c.c $(pkg-config --cflags --libs gstreamer-1.0) `pkg-config --cflags --libs gtk+-3.0`
+gcc -o Mu2c Mu2c.c $(pkg-config --cflags --libs gstreamer-1.0) `pkg-config --cflags --libs gtk+-3.0`
 
 # Remove old contents and add date and time to Version.ini
 echo "$(date +'%Y-%m-%d %H:%M:%S')" > Version.ini
